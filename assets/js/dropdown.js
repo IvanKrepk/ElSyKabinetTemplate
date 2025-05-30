210aeda6bcb0480c88b862d8c9c3c19966230a3f
IvanKrepk <i.am.krepkov.ivan@yandex.ru>
2025-05-30 18:41:14 +0300

    updated

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,6 +1,9 @@
 'use strict';
 
-let elsyDropdowns = document.querySelectorAll(".elsy-dropdown");
+document.addEventListener("DOMContentLoaded", function() {
+    //#region elsy-dropdown
+
+    let elsyDropdowns = document.querySelectorAll(".elsy-dropdown");
     elsyDropdowns.forEach(elsyDropDown => {
         let isFocused = false;
         let elsyDropDownSelected = elsyDropDown.querySelector(".dropdown-current");
@@ -8,10 +11,8 @@ let elsyDropdowns = document.querySelectorAll(".elsy-dropdown");
         let elsyDropDownArrow = elsyDropDownButton.querySelector(".dropdown-arrow");
         let elsyDropDownList = elsyDropDown.querySelector(".dropdown-list");
         let elsyDropDownListItems = elsyDropDownList.querySelectorAll("li");
-
         elsyDropDownList.style.borderTopLeftRadius = "0px";
         elsyDropDownList.style.borderTopRightRadius = "0px";
-
         const toggleArrow = () => {
             if (elsyDropDownList.classList.contains("hidden")) {
                 elsyDropDownArrow.style.borderRight = "var(--arrow-size) solid transparent";
@@ -25,13 +26,10 @@ let elsyDropdowns = document.querySelectorAll(".elsy-dropdown");
                 elsyDropDownArrow.style.borderBottom = "var(--arrow-size) solid black";
             }
         };
-
         const toggleDropdown = (toHide = null) => {
             (toHide == null) ? elsyDropDownList.classList.toggle("hidden") : 
                 elsyDropDownList.classList.toggle("hidden", toHide);
-
             toggleArrow();
-
             if (elsyDropDownList.classList.contains("hidden")) {
                 elsyDropDownButton.style.borderBottomWidth = "0px";
                 elsyDropDownList.style.borderTopWidth = "1px";
@@ -48,13 +46,11 @@ let elsyDropdowns = document.querySelectorAll(".elsy-dropdown");
                 elsyDropDownButton.style.borderBottomRightRadius = "0px"; 
             }
         };
-
         elsyDropDownButton.addEventListener("click", () => {
             toggleDropdown();
             elsyDropDown.style.borderColor = "var(--color-border-focused)";
             elsyDropDownList.style.borderColor = "var(--color-border-focused)";
         });
-
         elsyDropDownButton.addEventListener("blur", () => {
             isFocused = false;
             setTimeout(() => { 
@@ -65,7 +61,6 @@ let elsyDropdowns = document.querySelectorAll(".elsy-dropdown");
                 }
             }, 150);
         });
-
         elsyDropDownListItems.forEach(item => {
             item.addEventListener("click", () => {
                 elsyDropDownSelected.textContent = item.textContent;
@@ -74,3 +69,48 @@ let elsyDropdowns = document.querySelectorAll(".elsy-dropdown");
             })
         });
     });
+
+    //#endregion accounts-translate-dropdown
+
+    let accountsTranslateDropdowns = document.querySelectorAll(".accounts-translate-dropdown");
+    
+    accountsTranslateDropdowns.forEach(accountsTranslateDropdown => {
+        let inputField = accountsTranslateDropdown.querySelector(".dropdown-field");
+        let txtCurrent = accountsTranslateDropdown.querySelector(".dropdown-field-value");
+        let btnClose = accountsTranslateDropdown.querySelector(".close-button");
+        let arrow = accountsTranslateDropdown.querySelector(".dropdown-arrow");
+        let lstAccounts = accountsTranslateDropdown.querySelector(".dropdown-list");
+        let accountItems = lstAccounts.querySelectorAll("li");
+
+        accountItems.forEach(accountItem => {
+            accountItem.addEventListener("click", () => {
+                txtCurrent.value = accountItem.textContent;
+                btnClose.classList.remove("hidden");
+            });
+        });
+
+        txtCurrent.addEventListener("focus", () => {
+            lstAccounts.classList.toggle("hidden");
+        });
+
+        txtCurrent.addEventListener("blur", () => {
+            setTimeout(() => {
+                lstAccounts.classList.toggle("hidden");
+            }, 200);
+        });
+
+        arrow.addEventListener("click", () => {
+            if (lstAccounts.classList.contains("hidden")) {
+                txtCurrent.focus();
+            }
+        });
+
+        btnClose.addEventListener("click", () => {
+            txtCurrent.value = "";
+            btnClose.classList.toggle("hidden");
+        });
+    });
+
+    //#region 
+});
+
